63e59d69745691e7b9bce02ac1c14a158a1c959c
Aidar Sharafiev <Aidar_Sharafiev@epam.com>
2021-11-01 21:20:57 +0300

    feat(spring-boot): added task 1 CommandLineRunner

---
+++ commandLineAppplication/src/test/java/com/rostertwo/commandLineAppplication/CommandLineAppplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rostertwo.commandLineAppplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandLineAppplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
